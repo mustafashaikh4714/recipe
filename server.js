da013cab4890a1ff3f624b061ca34b4adfa337c8
kiranbhalerao123 <daghalerahul0@gmail.com>
2018-12-29 22:07:32 +0530

    new changes

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(__dirname + "/dist"));
+app.use(express.static(__dirname + "/dist/Project"));
 
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "dist/index.html"))
+    res.sendFile(path.join(__dirname, "dist/Project/index.html"))
   })
-  
+
 app.listen(process.env.PORT || 8080);
