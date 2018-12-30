022b01fef08f05a8ceede3b8c9e0f953d4cdf518
kiranbhalerao123 <daghalerahul0@gmail.com>
2018-12-30 12:25:06 +0530

    deploy to github

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const express = require('express');
-const app = express();
-const path = require('path');
+// const express = require('express');
+// const app = express();
+// const path = require('path');
 
-app.use(express.static(__dirname + "/dist/Project"));
+// app.use(express.static(__dirname + "/dist/Project"));
 
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "dist/Project/index.html"))
-  })
+// app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "dist/Project/index.html"))
+//   })
 
-app.listen(process.env.PORT || 8080);
+// app.listen(process.env.PORT || 8080);
